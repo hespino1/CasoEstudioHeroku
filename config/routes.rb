3d66f1ad7d5aa987987bf234ed5415e6affa0e3c
Hugo E <hespino1@eafit.edu.co>
2012-08-29 22:54:28 -0500

    mejora links

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Store::Application.routes.draw do
 
   resources :products
 
+  get "pages/products"
+
+  get "pages/purchases"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
